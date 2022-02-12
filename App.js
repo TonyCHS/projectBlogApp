1c68ee5a004e123e605238ab3c167ab06f472f4d
TonyCHS <92799152+TonyCHS@users.noreply.github.com>
2022-02-12 09:43:00 +0800

    commit 1: More  npm install

--- App.js
+++ App.js
@@ -2,21 +2,40 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import SignInScreen from "./screens/SignInScreen";
-import AccountScreen from "./screens/AccountScreen";
-import { NavigationContainer } from "@react-navigation/native";
+import SignUpScreen from "./screens/SignUpScreen";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import TabStack from "./components/TabStack";
+import { useSelector, useDispatch, Provider } from "react-redux";
+import { signInAction } from "./redux/ducks/blogAuth";
+import store from "./redux/createStore";
+import { toggleDarkMode } from "./redux/ducks/accountPrefs";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+function App() {
   const [loading, setLoading] = useState(true);
-  const [signedIn, setSignedIn] = useState(false);
+  const dispatch = useDispatch();
+  const signedIn = useSelector((state) => state.auth.signedIn);
+  const isDarkModeOn = useSelector((state) => state.prefs.darkMode);
 
   async function loadToken() {
     const token = await AsyncStorage.getItem("token");
     if (token) {
-      setSignedIn(true);
+      dispatch(signInAction());
     }
     setLoading(false);
   }
@@ -25,20 +44,28 @@ export default function App() {
     loadToken();
   }, []);
 
-  return loading ? (
-    <View style={styles.container}>
-      <ActivityIndicator />
-    </View>
-  ) : (
-    <NavigationContainer>
-      <Stack.Navigator
-        mode="modal"
-        headerMode="none"
-        initialRouteName={signedIn ? "Account" : "SignIn"}
-      >
-        <Stack.Screen component={AccountScreen} name="Account" />
-        <Stack.Screen component={SignInScreen} name="SignIn" />
-      </Stack.Navigator>
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer theme={isDarkModeOn ? DarkTheme : DefaultTheme}>
+      {signedIn ? (
+        <TabStack />
+      ) : (
+        <Stack.Navigator
+          mode="modal"
+          headerMode="none"
+          screenOptions={{ animationEnabled: false }}
+        >
+          <Stack.Screen component={SignInScreen} name="SignIn" />
+          <Stack.Screen component={SignUpScreen} name="SignUp" />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
